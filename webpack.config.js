1a013cb1bf505fd20e8dc5146f0a4f78e0a13229
Po Chen <chenpaul914@gmail.com>
2016-03-02 14:07:10 +0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    core: './core.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    preLoaders: [
+      { test: /\.js$/, loader: 'eslint-loader' }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['ng-annotate', 'babel?presets[]=es2015']
+      }
+    ]
+  },
+  eslint: {
+    fix: true,
+    configFile: './.eslintrc',
+    emitWarning: true,
+    emitError: true,
+    failOnWarning: false,
+    failOnError: true
+  },
+  watchOptions: {
+    poll: true,
+    aggregateTimeout: 1000
+  }
+};
